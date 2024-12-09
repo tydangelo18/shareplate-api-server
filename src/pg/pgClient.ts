4254854d83a62fc0205d806fbd6941a651dcbf0d
Ty D'Angelo <tydangelo18@gmail.com>
2024-12-09 16:16:09 -0600

    Extract Postgres connection logic out of server.ts

--- src/pg/pgClient.ts
+++ src/pg/pgClient.ts
@@ -1,11 +1,14 @@
 import { Client } from "pg";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 const client = new Client({
   user: process.env.POSTGRES_USER,
   host: process.env.POSTGRES_HOST,
   database: process.env.POSTGRES_DATABASE,
-  password: process.env.POSTGRES_PASSWORD,
+  password: String(process.env.POSTGRES_PASSWORD),
   port: Number(process.env.POSTGRES_PORT),
 });
 
-module.exports = client;
+export default client;
